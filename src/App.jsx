07b1302df6dc3d988cb6427b112709395e7dedbe
KevinAlves55 <programador.kevin4773@gmail.com>
2022-12-08 14:58:54 -0300

    refactor: Excluindo arquivos desnecessários

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { Post } from "./Post";
+
+export const App = () => {
+  return (
+    <div>
+      <Post 
+        author="Kevin Alves" 
+        content="Esse conteúdo é do POST"
+      />
+      <Post 
+        author="Kevin Alves" 
+        content="Esse conteúdo é do POST"
+      />
+      <Post 
+        author="Kevin Alves" 
+        content="Esse conteúdo é do POST"
+      />
+      <Post 
+        author="Kevin Alves" 
+        content="Esse conteúdo é do POST"
+      />
+      <Post 
+        author="Kevin Alves" 
+        content="Esse conteúdo é do POST"
+      />
+      <Post 
+        author="Kevin Alves" 
+        content="Esse conteúdo é do POST"
+      />
+    </div>
+  );
+};
