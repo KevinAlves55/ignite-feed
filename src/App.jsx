389739544f5429f7a0407c6a58c53d48fd3688b8
KevinAlves55 <programador.kevin4773@gmail.com>
2022-12-12 15:59:34 -0300

    feat: Criando a estrutura e a estilização do POST

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import "./styles/Global.css";
 import Style from "./App.module.css";
 
 import { Header } from "./components/Header";
-import { Post } from "./Post";
+import { Post } from "./components/Post/Post";
 import { Sidebar } from "./components/Sidebar";
 
 export const App = () => {
---
+++ src/components/Post/Post.jsx
@@ -0,0 +1,36 @@
+import Styles from "./Post.module.css";
+
+export const Post = () => {
+  return(
+    <article className={Styles.post}>
+      <header>
+        <div className={Styles.author}>
+          <img src="https://avatars.githubusercontent.com/u/89947057?v=4" />
+
+          <div className={Styles.authorInfo}>
+            <strong>Kevin Alves Da Silva</strong>
+            <span>Dev Front-End</span>
+          </div>
+        </div>
+
+        <time title="11 de maio de 2021 às 08:13" dateTime="2022-05-11 08:00:00">Publicado há 1h</time> 
+      </header>
+
+      <div className={Styles.content}>
+          <p>Fala galeraa 👋</p>
+
+          <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+
+          <p>
+            <a href="">jane.design/doctorcare</a>
+          </p>
+
+          <p>
+            <a href="">#novoprojeto</a>{" "}
+            <a href="">#nlw </a>{" "}
+            <a href="">#rocketseat</a>
+          </p>
+      </div>
+    </article>
+  );
+};
