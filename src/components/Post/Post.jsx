e3478dc7b526b334a397d6b8d4f7ebac96587517
KevinAlves55 <programador.kevin4773@gmail.com>
2022-12-12 16:24:12 -0300

    feat: Finalizado a estrutura e estilização de comentar

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -31,6 +31,18 @@ export const Post = () => {
             <a href="">#rocketseat</a>
           </p>
       </div>
+
+      <form className={Styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+
+        <textarea 
+          placeholder="Deixe um comentário"
+        />
+
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
     </article>
   );
 };
